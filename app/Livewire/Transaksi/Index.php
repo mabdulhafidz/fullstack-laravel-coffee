8f8033eb55e3c55a7b35d01331e5b12c3fdb7ad1
M Abdul Hafidz <hfidz7534@gmail.com>
2024-02-20 02:56:25 +0700

    feat(2lyffj2y) Transaction Delete, Update, Add

--- app/Livewire/Transaksi/Index.php
+++ app/Livewire/Transaksi/Index.php
@@ -11,14 +11,112 @@ use Livewire\Component;
 
 class Index extends Component
 {
+
+    public $cart = [];
+    public $totalPrice =  0;
+    public $itemCount =  0;
+    public $selectedCategory;
+
+
     public function render()
     {
-       $menus = Menu::all();
-       $categories = Category::all();
-       $customers = Customer::all();
-       $employees = Employee::all();
-       $stocks = Stock::all();
-        return view('livewire.transaksi.index', compact('menus','categories', 'employees', 'stocks'));
+        $menus = Menu::all();
+        $categories = Category::all();
+        $customers = Customer::all();
+        $employees = Employee::all();
+        $stocks = Stock::all();
+        $categories = Category::all();
+        $menus = Menu::when($this->selectedCategory, function ($query) {
+            return $query->where('category_id', $this->selectedCategory);
+        })->get();
+        return view('livewire.transaksi.index', compact('menus', 'categories', 'employees', 'stocks'));
+    }
+
+    public function addToCart($menuId, $menuName, $price, $quantity =  1)
+    {
+        $stock = Stock::where('menu_id', $menuId)->first();
+    
+       
+        if ($stock && $stock->jumlah >= $quantity) {
+            $stock->decrement('jumlah', $quantity);
+    
+            if (array_key_exists($menuId, $this->cart)) {
+              
+                $this->cart[$menuId]['qty'] += $quantity;
+            } else {
+                $this->cart[$menuId] = [
+                    'id' => $menuId,
+                    'name' => $menuName,
+                    'price' => $price,
+                    'stock' => $stock,
+                    'qty' => $quantity,
+                ];
+            }
+    
+            $this->totalPrice += $price * $quantity;
+            $this->itemCount += $quantity;
+    
+        } else {
+            $this->emit('stockEmpty', $menuName);
+        }
     }
     
+
+    
+    
+
+    protected $listeners = ['stockEmpty' => 'handleStockEmpty'];
+
+    public function handleStockEmpty($menuName)
+    {
+        $this->addError('stockEmpty', "$menuName is out of stock.");
+    }
+
+
+
+    public function removeFromCart($menuId)
+    {
+    if (array_key_exists($menuId, $this->cart)) {
+     
+        $price = $this->cart[$menuId]['price'];
+        $qty = $this->cart[$menuId]['qty'];
+        unset($this->cart[$menuId]);
+
+        $this->totalPrice -= $price * $qty;
+        $this->itemCount -= $qty;
+
+        $this->restoreStock($menuId, $qty);
+    }
+    }
+
+    private function restoreStock($menuId, $quantity)
+    {
+        $stock = Stock::where('menu_id', $menuId)->first();
+
+        if ($stock) {
+            $stock->increment('jumlah', $quantity);
+        }
+    }
+
+    public function updateCart($menuId, $qty)
+    {
+        if (array_key_exists($menuId, $this->cart)) {
+        $price = $this->cart[$menuId]['price'];
+        $oldQty = $this->cart[$menuId]['qty'];
+        $this->cart[$menuId]['qty'] = $qty;
+
+        $this->totalPrice = $this->totalPrice - $price * $oldQty + $price * $qty;
+        $this->itemCount += $qty - $oldQty;
+    }
+}
+
+    public function calculateCart()
+    {
+        $this->totalPrice =  0;
+        $this->itemCount =  0;
+        foreach ($this->cart as $item) {
+            $this->totalPrice += $item['price'] * $item['qty'];
+            $this->itemCount += $item['qty'];
+        }
+    }
 }
